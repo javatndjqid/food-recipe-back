d9d805add0f277ac493ce11679ba44a74077b7ab
javatndjqid <javatndjqid@gmail.com>
2021-04-08 12:41:38 +0900

    연결 주소 ec2로 수정

--- src/main/java/com/example/search/MarketClient.java
+++ src/main/java/com/example/search/MarketClient.java
@@ -3,8 +3,8 @@ package com.example.search;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "market", url = "http://192.168.0.27:8080")
-//@FeignClient(name = "market", url = "http://ec2-13-124-253-19.ap-northeast-2.compute.amazonaws.com:8080")
+//@FeignClient(name = "market", url = "http://192.168.0.27:8080")
+@FeignClient(name = "market", url = "http://ec2-13-124-253-19.ap-northeast-2.compute.amazonaws.com:8080")
 public interface MarketClient {
 	@GetMapping("/product")
 	String getProduct();
