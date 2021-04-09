8c43833bd127462cb5761c14341989cc8599fe29
javatndjqid <javatndjqid@gmail.com>
2021-04-09 17:34:38 +0900

    0409

--- src/main/java/com/example/search/RecipeClient.java
+++ src/main/java/com/example/search/RecipeClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "recipe", url = "http://192.168.0.29:8080")
-@FeignClient(name = "recipe", url = "http://ec2-13-209-41-162.ap-northeast-2.compute.amazonaws.com:8080")
+@FeignClient(name = "recipe", url = "http://192.168.0.29:8080")
+//@FeignClient(name = "recipe", url = "http://ec2-13-209-41-162.ap-northeast-2.compute.amazonaws.com:8080")
 public interface RecipeClient {
 	@GetMapping("/recipes/{recipeId}")
 	String getRecipeDetail(@PathVariable("recipeId") long recipeId);
